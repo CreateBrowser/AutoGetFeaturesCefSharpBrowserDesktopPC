be1c8f74b962cb6c8f073a01a7932ef38a5b1d95
Mohammed Osama <mohamedosama914@users.noreply.github.com>
2018-04-20 14:21:14 +0200

    Update Example.cs

--- Example.cs
+++ Example.cs
@@ -1,1 +1,8 @@
-Example Chromium CefSharp 63.0.3 Browser .sln
+Example Chromium CefSharp 63.0.3 Browser 
+Chromium Application.sln
+Application Settings System
+Group Custom Path
+
+/*
+ <startup> <supportedRuntime version="v4.0" sku=".NETFramework,Version=v4.7" /> </startup>
+*/
