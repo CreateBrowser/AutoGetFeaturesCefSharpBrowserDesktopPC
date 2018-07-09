8ff8528e244bbbe1631a4a46d60247c863753139
Mohammed Osama <mohamedosama914@users.noreply.github.com>
2018-07-09 16:01:26 +0200

    Update Example.cs

--- Example.cs
+++ Example.cs
@@ -6,3 +6,9 @@ Group Custom Path Configuration Desktop PC
 /* <startup> <supportedRuntime version="v4.0" sku=".NETFramework,Version=v4.7" /> </startup>*/
 
 Start the browser after initialize global component CefSharp v65.0.0-pre01
+
+
+
+     
+  <CS> <script async src=""></script>    </CS>
+        <img alt="" style="max-width:160px;" src="https://www.keepvid.info/images/paypay%402x.png" >
