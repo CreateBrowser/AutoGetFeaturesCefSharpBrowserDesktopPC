431b6326db2b951d3a0f7f21ff697eeb0e880bbb
Mohammed Osama <mohamedosama914@users.noreply.github.com>
2018-07-11 18:07:41 +0200

    Update Example.cs

--- Example.cs
+++ Example.cs
@@ -10,5 +10,50 @@ Start the browser after initialize global component CefSharp v65.0.0-pre01
 
 
      
-  <CS> <script async src=""></script>    </CS>
-        <img alt="" style="max-width:160px;" src="https://www.keepvid.info/images/paypay%402x.png" >
+  <cs> <script async src=""></script>  |  <img alt="" style="max-width:160px;" src="https://www.keepvid.info/images/paypay%402x.png" > | </cs>
+        
+    |    <cs> DownloadHandler.cs <cs>
+        
+        <cs>
+        public class DownloadHandler : IDownloadHandler
+    {
+        public event EventHandler<DownloadItem> OnBeforeDownloadFired;
+
+        public event EventHandler<DownloadItem> OnDownloadUpdatedFired;
+
+        public void OnBeforeDownload(IBrowser browser, DownloadItem downloadItem, IBeforeDownloadCallback callback)
+        {
+            var handler = OnBeforeDownloadFired;
+
+            if (handler != null)
+            {
+                handler(this, downloadItem);
+            }
+
+            if (!callback.IsDisposed)
+            {
+                using (callback)
+                {
+                    callback.Continue(downloadItem.SuggestedFileName, showDialog: true);
+
+                }
+            }
+        }
+
+        public void OnDownloadUpdated(IBrowser browser, DownloadItem downloadItem, IDownloadItemCallback callback)
+        {
+            /*
+            if (downloadItem.IsComplete || downloadItem.IsCancelled)
+            {
+                //do stuff
+            }
+            */
+
+            var handler = OnDownloadUpdatedFired;
+            if (handler != null)
+            {
+                handler(this, downloadItem);
+            }
+        }
+    }
+        <cs> |
