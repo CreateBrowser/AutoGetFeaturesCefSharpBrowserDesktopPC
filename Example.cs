f7a55d7fc042668f925a5191d2a4f662c6461e88
Mohammed Osama <mohamedosama914@users.noreply.github.com>
2018-07-19 15:21:34 +0200

    Update Example.cs

--- Example.cs
+++ Example.cs
@@ -1,11 +1,12 @@
-Example Chromium Browser CefSharp v63.0.3 ^ v65.0.0-pre01
+Example Chromium Browser CefSharp v63.0.3 ^ | Pro | v65.0.0-pre01 
 Chromium Application.sln
 Application Settings System
 Group Custom Path Configuration Desktop PC 
 
+Start the browser after initialize global component CefSharp v65.0.0-pre01
+
 /* <startup> <supportedRuntime version="v4.0" sku=".NETFramework,Version=v4.7" /> </startup>*/
 
-Start the browser after initialize global component CefSharp v65.0.0-pre01
 
 | CustomMenuHandler |
 
