435fc45d3fb37776245788df29aaab0045af956d
Mohammed Osama Mohamed Sayed Ahmed <mohamedosama914@users.noreply.github.com>
2019-02-10 15:19:45 +0200

    Update No Example.cs

--- No Example.cs
+++ No Example.cs
@@ -1,38 +1,35 @@
+// Example Custom Desktop Project C# CefSharp A Example Auto Get Fast Browser 
+
+// (Register now for your free entry ticket!) https://engine.passapp.io/ict-form-embed
+
+<cs> - Example Custom Desktop Project C# CefSharp A Example Auto Get Fast Browser - </cs>
+
+Example Chromium Browser CefSharp v63.0.3 | Pro | v65.0.0-pre01 | Pro | v69.0.0-pre01
+Chromium Application.sln
+Application Settings System
+Group Custom Path Configuration Desktop PC 
+<VB>
+New | Example Custom Desktop Project C# CefSharp A Example Auto Get Fast Browser
+</VB>
+Start the browser after initialize global component CefSharp v65.0.0-pre01
+"GO"
+
+\\\\\\\\\\\\\\\
+
+\\\\\\\\\\\\\\\
+
+
+|GO|
+       
+
+/* <startup> <supportedRuntime version="v4.0" sku=".NETFramework,Version=v4.7" /> </startup>*/
+
 <cs> 
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
-No Example.cs
+No Example 
+cs
+// Example Custom Desktop Project C# CefSharp A Example Auto Get Fast Browser 
+
+// (Register now for your free entry ticket!) https://engine.passapp.io/ict-form-embed
+
+
 </cs>
