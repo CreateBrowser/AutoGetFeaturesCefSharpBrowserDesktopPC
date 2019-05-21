25116aa5ddffe1e6850abf5e2792c95972d4fee0
Mohammed Osama Mohamed Sayed Ahmed <mohamedosama914@users.noreply.github.com>
2019-05-21 16:57:10 +0200

    Update Example.cs

--- Example.cs
+++ Example.cs
@@ -4,8 +4,8 @@
 
 <cs> - Example Custom Desktop Project C# CefSharp A Example Auto Get Fast Browser - </cs>
 
-Example Chromium Browser | Release CefSharp Version | 63.0.3 | 71.0.2 (71.0.3578.98) | 73.0.130
-Example Chromium Browser | Release Pro CefSharp Version | v65.0.0-pre01 | Pro | v69.0.0-pre01 |  v73.0.120-pre01 |
+Example Chromium Browser | Release CefSharp Version | 63.0.3 | 71.0.2 (71.0.3578.98) | 73.0.130 | 
+Example Chromium Browser | Release Pro CefSharp Version | v65.0.0-pre01 | v69.0.0-pre01 | v73.0.120-pre01 | 
 
  Chromium Application.sln
 Application Settings System
